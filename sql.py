5ed7eae5a60d8528641df8daaeac59e2d36fc580
techception <alexhall1993@hotmail.co.uk>
2026-02-08 13:07:24 +0000

    drill

--- sql.py
+++ sql.py
@@ -1,5 +1,14 @@
 import pandas as pd
-import sqlalchemy 
+from sqlalchemy import create_engine, text
 from urllib.parse import quote_plus
 
+engine = create_engine('sqlite:///practice.db')
 
+"""
+with engine.connect() as conn:
+    result = conn.execute(text('SELECT * FROM user'))
+    print(result.fetchone())
+"""
+
+df = pd.read_sql('SELECT * FROM user',engine)
+print(df)
