ae219ef6e1855c059a55989cdef649c9e495dc27
techception <alexhall1993@hotmail.co.uk>
2026-02-13 18:46:49 +0000

    chapter 2 comp

--- book/chapter2/try.py
+++ book/chapter2/try.py
@@ -1,10 +1,23 @@
+#2-3
+name = "Lorraine"
+print(f"Hello {name}, would you like to learn some python today?")
+
+
+#2-4
+name = "robyn";
+print(name)
+print(name.lower());
+print(name.upper());
+print(name.title());
+
+
 name = "Anne"
 message = f"hello {name} would you like to learn some python today?"
 print(message)
 
 
 ask = 'would you like to learn some python today?'
-'#lowercase'
+#lowercase
 print('#lowercase')
 print(f"hello {name.lower()}", ask)
 
@@ -16,3 +29,18 @@ print(f"hello {name.upper()}", ask)
 print('#titlecase')
 print(f"hello {name.title()}", ask)
 print(message.title())
+
+#2-5
+print('Descarte once said "I think therefore I am".');
+
+#2-6
+famous_person = 'Descarte'; 
+message = f'{famous_person} once said "I think therefore I am"'
+print(message)
+
+#2-7
+name = "\n\tAlex\t\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip());
