ab6315c8b1b930b67e82873a6ca7daf1ed4a7e9a
techception <alexhall1993@hotmail.co.uk>
2026-02-13 18:32:10 +0000

    chapter 2

---
+++ book/chapter2/try.py
@@ -0,0 +1,18 @@
+name = "Anne"
+message = f"hello {name} would you like to learn some python today?"
+print(message)
+
+
+ask = 'would you like to learn some python today?'
+'#lowercase'
+print('#lowercase')
+print(f"hello {name.lower()}", ask)
+
+#uppercase
+print('#uppercase')
+print(f"hello {name.upper()}", ask)
+
+#titlecase 
+print('#titlecase')
+print(f"hello {name.title()}", ask)
+print(message.title())
