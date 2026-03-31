45bc47ccbcae17958e451ce60c581953f48d2a18
techception <alexhall1993@hotmail.co.uk>
2026-03-31 18:57:49 +0100

    aliens 5-4

--- book/chapter5/two.py
+++ book/chapter5/two.py
@@ -23,5 +23,46 @@ win = alien_color == "red";
 if win:
     print("You just earned 5 points")
 print("tested if alien_colour == 'green'")
+###############################################################################
 
+
+
+#5-4
+###############################################################################
+ex = "Alien Colors #2:"
+desc = """
+    Chose a colour for an aliean as you did in Exercise 5-3, and write an 
+    if-else chain.
+    """
+
+sub1 = """If the alien's color is green, print a statement that the player just 
+earned 5 points for shooring the alien"""
+
+sub2 = """If the alien's color isn't green, print a statement that the player 
+just earned 10 points """
+
+sub3 = """Write one version of this program that runs the if block and another 
+that runs the else block"""
+
+print(ex);
+print(desc);
+
+print(sub1)
+print(sub2)
+print(sub3)
+
+aliens = ["purple", "GREEN"];
+i = 0 
+while i < len(aliens):
+    print("\n")
+    print("test",i)
+    green = aliens[i].lower() == "green"
+    if green: 
+        print(sub1)
+        print("You Just Earned 5 Points!")
+    else:
+        print(sub2)
+        print("You Just Earned 10 Points!")
+
+    i += 1; 
 
