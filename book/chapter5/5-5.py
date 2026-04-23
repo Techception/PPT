d70c1c6f6498ee884ae5101cc8118577f820a816
techception <alexhall1993@hotmail.co.uk>
2026-04-23 18:54:17 +0100

    lesson 5.5 from the book making an if elif else chain

---
+++ book/chapter5/5-5.py
@@ -0,0 +1,71 @@
+#5-5
+###############################################################################
+ex = "Alien Colors #3:"
+desc = "Turn your if-else chain from Exercise 5-4 into an if-elif-else chain."
+
+print(desc);
+
+
+print("\n")
+alien = "black"
+colour = alien.lower()
+if colour == "green": 
+    print("the alien is", alien)
+    print("You Just Earned 5 Points!")
+elif colour == "yellow":
+    print("the alien is", alien)
+    print("You Just Earned 10 Points!")
+elif colour == "red":
+    print("the alien is", alien)
+    print("You Just Earned 15 Points!")
+else:
+    print("the alien is", alien)
+
+print("\n")
+alien = "rEd"
+colour = alien.lower()
+if colour == "green": 
+    print("the alien is", alien)
+    print("You Just Earned 5 Points!")
+elif colour == "yellow":
+    print("the alien is", alien)
+    print("You Just Earned 10 Points!")
+elif colour == "red":
+    print("the alien is", alien)
+    print("You Just Earned 15 Points!")
+else:
+    print("the alien is", alien)
+
+
+print("\n")
+alien = "GreeN"
+colour = alien.lower()
+if colour == "green": 
+    print("the alien is", alien)
+    print("You Just Earned 5 Points!")
+elif colour == "yellow":
+    print("the alien is", alien)
+    print("You Just Earned 10 Points!")
+elif colour == "red":
+    print("the alien is", alien)
+    print("You Just Earned 15 Points!")
+else:
+    print("the alien is", alien)
+
+
+print("\n")
+alien = "yellow"
+colour = alien.lower()
+if colour == "green": 
+    print("the alien is", alien)
+    print("You Just Earned 5 Points!")
+elif colour == "yellow":
+    print("the alien is", alien)
+    print("You Just Earned 10 Points!")
+elif colour == "red":
+    print("the alien is", alien)
+    print("You Just Earned 15 Points!")
+else:
+    print("the alien is", alien)
+
+
