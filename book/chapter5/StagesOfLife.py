2d68918288c5b790e15570f44750821f530f48b9
techception <alexhall1993@hotmail.co.uk>
2026-06-06 18:09:56 +0100

    improvethe text

--- book/chapter5/StagesOfLife.py
+++ book/chapter5/StagesOfLife.py
@@ -8,20 +8,31 @@ task1 = (
         "person is a baby"
         )
 
-task2 = """If the person is at lease 4 years old but less and 13, print a 
-    message thatthe person is a kid
-"""
+task2 = (
+        "If the person is at lease 4 years old but less and 13, print a "
+        "message thatthe person is a kid"
+        )
 
-task3 = """ If the person is at least 13 years old but less than 20, print a 
-    message that the person is a teenager.
-"""
+task3 = (
+        "If the person is at least 13 years old but less than 20, print a "
+        "message that the person is a teenager."
+        )
 
-task4 = """ If the person is at least 20 years old but less than 65, print a 
-    message that the person is an adult.
-"""
+task4 = ( 
+        "If the person is at least 20 years old but less than 65, print a "
+        "message that the person is an adult."
+        )
 
-task5 = """If the person is age 65 or older, print a messeage that the person 
-    is an elder.
-"""
+task5 = (
+        "If the person is age 65 or older, print a messeage that the person "
+        "is an elder."
+        )
 
-print(task, "\n\t", task1, "\n\t", task2, "\n\t", task3, "\n\t", task4, "\n\t", task5)
+print(
+    task, 
+    "\n\t"+task1, 
+    "\n\t"+task2, 
+    "\n\t"+task3, 
+    "\n\t"+task4, 
+    "\n\t"+task5
+    )
