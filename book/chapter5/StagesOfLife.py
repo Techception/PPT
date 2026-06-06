b24d4e0924a2694148dcd5529dafe64ea30a1334
techception <alexhall1993@hotmail.co.uk>
2026-06-06 17:58:35 +0100

    ex 5.6 stages of life start

---
+++ book/chapter5/StagesOfLife.py
@@ -0,0 +1,26 @@
+task = """
+    write an if else chan that determines a persons stage of life. 
+    Set a value for the varidable age, and then:
+"""
+
+task1 = """If the person is less than 2 years old. print a message that the 
+    person is a baby
+"""
+
+task2 = """If the person is at lease 4 years old but less and 13, print a 
+    message thatthe person is a kid
+"""
+
+task3 = """ If the person is at least 13 years old but less than 20, print a 
+    message that the person is a teenager.
+"""
+
+task4 = """ If the person is at least 20 years old but less than 65, print a 
+    message that the person is an adult.
+"""
+
+task5 = """If the person is age 65 or older, print a messeage that the person 
+    is an elder.
+"""
+
+print(task, "\n\t", task1, "\n\t", task2, "\n\t", task3, "\n\t", task4, "\n\t", task5)
