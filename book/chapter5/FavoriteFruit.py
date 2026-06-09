48a9da97b32209f4857b1328d522944f56912f67
techception <alexhall1993@hotmail.co.uk>
2026-06-09 18:10:13 +0100

    stated on 5-7

---
+++ book/chapter5/FavoriteFruit.py
@@ -0,0 +1,6 @@
+task=(
+    "Make a list of your favorite fruits and then write a series of" 
+    "independent if statements that check for certain fruits in your list"
+)
+
+favFruits = ["Pineapple", "Mango", "Tomato"]
